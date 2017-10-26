14a583ebd956c85dfd2115ac3726b797359c2f80
Mariusz Woloszyn <emsi>
2017-10-26 17:05:28 +0200

    Comments

--- gcloud/install.sh
+++ gcloud/install.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 echo "Checking for CUDA and installing."
+
+# Instaling docker
+apt-get update && apt-get -y install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get update
+apt-get install -y docker-ce
+
 # Check for CUDA and try to install.
 cd /root
 if ! dpkg-query -W cuda-8-0; then
@@ -9,27 +17,26 @@ apt-get update
 apt-get install cuda-8-0 -y
 fi
 
-apt-get update && apt-get -y install apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-apt-get update
-apt-get install -y docker-ce
-
+# Install nvidia-docker
 wget -P /tmp https://github.com/NVIDIA/nvidia-docker/releases/download/v1.0.1/nvidia-docker_1.0.1-1_amd64.deb
 dpkg -i /tmp/nvidia-docker*.deb && rm /tmp/nvidia-docker*.deb
 git clone https://github.com/emsi/DL_Training_materials
 cd DL_Training_materials
 ./Docker/docker-build
 
+
+# Build emsi/dl_training docker
 mkdir /rev-proxy
 mkdir /rev-proxy/htpasswd
 mkdir /rev-proxy/certs
 cp ./Docker/nginx.tmpl /rev-proxy/nginx.tmpl
 
+# Configure training environment
 docker run -d -p 80:80 -p 443:443 --name nginx -v /rev-proxy/htpasswd:/etc/nginx/htpasswd -v /etc/nginx/conf.d -v /etc/nginx/vhost.d -v /usr/share/nginx/html -v /rev-proxy/certs/:/etc/nginx/certs:ro --label com.github.jrcs.letsencrypt_nginx_proxy_companion.nginx_proxy=true nginx
 
 docker run -d --name nginx-gen --volumes-from nginx -v /rev-proxy/nginx.tmpl:/etc/docker-gen/templates/nginx.tmpl:ro -v /var/run/docker.sock:/tmp/docker.sock:ro jwilder/docker-gen -notify-sighup nginx -watch -wait 5s:30s /etc/docker-gen/templates/nginx.tmpl /etc/nginx/conf.d/default.conf
 
 docker run -d --name nginx-letsencrypt -e "NGINX_DOCKER_GEN_CONTAINER=nginx-gen" --volumes-from nginx -v /rev-proxy/certs/:/etc/nginx/certs:rw -v /var/run/docker.sock:/var/run/docker.sock:ro jrcs/letsencrypt-nginx-proxy-companion
 
+# emsi/dl_training docker
 docker run -d --name dl-google -h d-google -e "VIRTUAL_HOST=dl-google.qpqp01.pl" -e "LETSENCRYPT_HOST=dl-google.qpqp01.pl" -e "LETSENCRYPT_EMAIL=emsi@qpqp01.pl" -e "VIRTUAL_PORT=8888" emsi/dl_training
