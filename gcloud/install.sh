48dcb2f83850b9eadfdf8966ae2d0bda14ca7e60
Mariusz Woloszyn <emsi>
2017-10-26 18:55:48 +0200

    Delay start a bit...

--- gcloud/install.sh
+++ gcloud/install.sh
@@ -39,13 +39,12 @@ docker run -d --name nginx-gen --volumes-from nginx -v /rev-proxy/nginx.tmpl:/et
 docker run -d --name nginx-letsencrypt -e "NGINX_DOCKER_GEN_CONTAINER=nginx-gen" --volumes-from nginx -v /rev-proxy/certs/:/etc/nginx/certs:rw -v /var/run/docker.sock:/var/run/docker.sock:ro jrcs/letsencrypt-nginx-proxy-companion
 
 # emsi/dl_training docker
-NV_GPU=0 nvidia-docker run -d --name dl-google -h d-google -e "VIRTUAL_HOST=dl-google.qpqp01.pl" -e "LETSENCRYPT_HOST=dl-google.qpqp01.pl" -e "LETSENCRYPT_EMAIL=emsi@qpqp01.pl" -e "VIRTUAL_PORT=8888" emsi/dl_training
-
 docker run --rm -ti xmartlabs/htpasswd -m dl qpqp01 > /rev-proxy/htpasswd/dl-goole.qpqp01.pl
+NV_GPU=0 nvidia-docker run -d --name dl-google -h d-google -e "VIRTUAL_HOST=dl-google.qpqp01.pl" -e "LETSENCRYPT_HOST=dl-google.qpqp01.pl" -e "LETSENCRYPT_EMAIL=emsi@qpqp01.pl" -e "VIRTUAL_PORT=8888" emsi/dl_training
 
 cat >> /root/env-up << EOF
 # env-up script
-for i in `seq 1 8`; do
+for i in $(seq 1 8); do
 	echo $i
 	docker run --rm -ti xmartlabs/htpasswd -m dl qpqp01 > /rev-proxy/htpasswd/dl$i.qpqp01.pl
 	NV_GPU=$i nvidia-docker run -d --name dl$i -h dl$i -e "VIRTUAL_HOST=dl$i.qpqp01.pl" -e "LETSENCRYPT_HOST=dl$i.qpqp01.pl" -e "LETSENCRYPT_EMAIL=emsi@qpqp01.pl" -e "VIRTUAL_PORT=8888" emsi/dl_training
