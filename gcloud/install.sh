879c80a5bb91818cb9a5ad83ab2792504178ff27
Mariusz Woloszyn <emsi>
2017-10-27 22:28:58 +0200

    Reordered container starting

--- gcloud/install.sh
+++ gcloud/install.sh
@@ -8,6 +8,14 @@ add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(
 apt-get update
 apt-get install -y docker-ce
 
+
+# Configure training environment
+docker run -d -p 80:80 -p 443:443 --name nginx -v /rev-proxy/htpasswd:/etc/nginx/htpasswd -v /etc/nginx/conf.d -v /etc/nginx/vhost.d -v /usr/share/nginx/html -v /rev-proxy/certs/:/etc/nginx/certs:ro --label com.github.jrcs.letsencrypt_nginx_proxy_companion.nginx_proxy=true nginx
+
+docker run -d --name nginx-gen --volumes-from nginx -v /rev-proxy/nginx.tmpl:/etc/docker-gen/templates/nginx.tmpl:ro -v /var/run/docker.sock:/tmp/docker.sock:ro jwilder/docker-gen -notify-sighup nginx -watch -wait 5s:30s /etc/docker-gen/templates/nginx.tmpl /etc/nginx/conf.d/default.conf
+
+docker run -d --name nginx-letsencrypt -e "NGINX_DOCKER_GEN_CONTAINER=nginx-gen" --volumes-from nginx -v /rev-proxy/certs/:/etc/nginx/certs:rw -v /var/run/docker.sock:/var/run/docker.sock:ro jrcs/letsencrypt-nginx-proxy-companion
+
 # Check for CUDA and try to install.
 cd /root
 if ! dpkg-query -W cuda-8-0; then
@@ -31,14 +39,6 @@ mkdir /rev-proxy/htpasswd
 mkdir /rev-proxy/certs
 cp ./Docker/nginx.tmpl /rev-proxy/nginx.tmpl
 
-# Configure training environment
-docker run -d -p 80:80 -p 443:443 --name nginx -v /rev-proxy/htpasswd:/etc/nginx/htpasswd -v /etc/nginx/conf.d -v /etc/nginx/vhost.d -v /usr/share/nginx/html -v /rev-proxy/certs/:/etc/nginx/certs:ro --label com.github.jrcs.letsencrypt_nginx_proxy_companion.nginx_proxy=true nginx
-
-docker run -d --name nginx-gen --volumes-from nginx -v /rev-proxy/nginx.tmpl:/etc/docker-gen/templates/nginx.tmpl:ro -v /var/run/docker.sock:/tmp/docker.sock:ro jwilder/docker-gen -notify-sighup nginx -watch -wait 5s:30s /etc/docker-gen/templates/nginx.tmpl /etc/nginx/conf.d/default.conf
-
-docker run -d --name nginx-letsencrypt -e "NGINX_DOCKER_GEN_CONTAINER=nginx-gen" --volumes-from nginx -v /rev-proxy/certs/:/etc/nginx/certs:rw -v /var/run/docker.sock:/var/run/docker.sock:ro jrcs/letsencrypt-nginx-proxy-companion
-sleep 10
-
 # emsi/dl_training docker
 docker pull xmartlabs/htpasswd
 docker run --rm -ti xmartlabs/htpasswd -m dl qpqp01 > /rev-proxy/htpasswd/dl-goole.qpqp01.pl
@@ -46,7 +46,7 @@ NV_GPU=0 nvidia-docker run -d --name dl-google -h d-google -e "VIRTUAL_HOST=dl-g
 
 cat >> /root/env-up << EOF
 # env-up script
-for i in \$(seq 1 8); do
+for i in \$(seq 1 7); do
 	echo \$i
 	docker run --rm -ti xmartlabs/htpasswd -m dl qpqp01 > /rev-proxy/htpasswd/dl\$i.qpqp01.pl
 	NV_GPU=\$i nvidia-docker run -d --name dl\$i -h dl\$i -e "VIRTUAL_HOST=dl\$i.ltcpln.pl" -e "LETSENCRYPT_HOST=dl\$i.ltcpln.pl" -e "LETSENCRYPT_EMAIL=emsi@qpqp01.pl" -e "VIRTUAL_PORT=8888" emsi/dl_training
