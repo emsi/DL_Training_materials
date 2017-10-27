94f3c4c8db42cee01b264bd33f5d8f8656cc8815
Mariusz Woloszyn <emsi>
2017-10-27 11:09:55 +0200

    ANew domain. Escape variables. Wait for container

--- gcloud/install.sh
+++ gcloud/install.sh
@@ -37,17 +37,19 @@ docker run -d -p 80:80 -p 443:443 --name nginx -v /rev-proxy/htpasswd:/etc/nginx
 docker run -d --name nginx-gen --volumes-from nginx -v /rev-proxy/nginx.tmpl:/etc/docker-gen/templates/nginx.tmpl:ro -v /var/run/docker.sock:/tmp/docker.sock:ro jwilder/docker-gen -notify-sighup nginx -watch -wait 5s:30s /etc/docker-gen/templates/nginx.tmpl /etc/nginx/conf.d/default.conf
 
 docker run -d --name nginx-letsencrypt -e "NGINX_DOCKER_GEN_CONTAINER=nginx-gen" --volumes-from nginx -v /rev-proxy/certs/:/etc/nginx/certs:rw -v /var/run/docker.sock:/var/run/docker.sock:ro jrcs/letsencrypt-nginx-proxy-companion
+sleep 10
 
 # emsi/dl_training docker
+docker pull xmartlabs/htpasswd
 docker run --rm -ti xmartlabs/htpasswd -m dl qpqp01 > /rev-proxy/htpasswd/dl-goole.qpqp01.pl
-NV_GPU=0 nvidia-docker run -d --name dl-google -h d-google -e "VIRTUAL_HOST=dl-google.qpqp01.pl" -e "LETSENCRYPT_HOST=dl-google.qpqp01.pl" -e "LETSENCRYPT_EMAIL=emsi@qpqp01.pl" -e "VIRTUAL_PORT=8888" emsi/dl_training
+NV_GPU=0 nvidia-docker run -d --name dl-google -h d-google -e "VIRTUAL_HOST=dl-google.ltcpln.pl" -e "LETSENCRYPT_HOST=dl-google.ltcpln.pl" -e "LETSENCRYPT_EMAIL=emsi@qpqp01.pl" -e "VIRTUAL_PORT=8888" emsi/dl_training
 
 cat >> /root/env-up << EOF
 # env-up script
 for i in \$(seq 1 8); do
-	echo $i
-	docker run --rm -ti xmartlabs/htpasswd -m dl qpqp01 > /rev-proxy/htpasswd/dl$i.qpqp01.pl
-	NV_GPU=$i nvidia-docker run -d --name dl$i -h dl$i -e "VIRTUAL_HOST=dl$i.qpqp01.pl" -e "LETSENCRYPT_HOST=dl$i.qpqp01.pl" -e "LETSENCRYPT_EMAIL=emsi@qpqp01.pl" -e "VIRTUAL_PORT=8888" emsi/dl_training
+	echo \$i
+	docker run --rm -ti xmartlabs/htpasswd -m dl qpqp01 > /rev-proxy/htpasswd/dl\$i.qpqp01.pl
+	NV_GPU=\$i nvidia-docker run -d --name dl\$i -h dl\$i -e "VIRTUAL_HOST=dl\$i.ltcpln.pl" -e "LETSENCRYPT_HOST=dl\$i.ltcpln.pl" -e "LETSENCRYPT_EMAIL=emsi@qpqp01.pl" -e "VIRTUAL_PORT=8888" emsi/dl_training
 done
 EOF
 chmod +x /root/env-up
